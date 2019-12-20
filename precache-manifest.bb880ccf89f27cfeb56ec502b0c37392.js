1e23ec713668317093f3fd6cace3ecd933fdebf4
ehaffey <e-haffey@hotmail.com>
2019-12-20 16:04:48 -0500

    Updates

---
+++ precache-manifest.bb880ccf89f27cfeb56ec502b0c37392.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37c8ba1099739c25fb54ca0fec6d4bc",
+    "url": "/puppy-pal/index.html"
+  },
+  {
+    "revision": "d5e88fc23f6875aa4085",
+    "url": "/puppy-pal/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "a87f06f64a21e1027c7f",
+    "url": "/puppy-pal/static/css/main.1f514d9b.chunk.css"
+  },
+  {
+    "revision": "d5e88fc23f6875aa4085",
+    "url": "/puppy-pal/static/js/2.c1ed8a11.chunk.js"
+  },
+  {
+    "revision": "a87f06f64a21e1027c7f",
+    "url": "/puppy-pal/static/js/main.cb629ad9.chunk.js"
+  },
+  {
+    "revision": "fbdf927508ca09ba9aa2",
+    "url": "/puppy-pal/static/js/runtime~main.6ff48b30.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/puppy-pal/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puppy-pal/static/media/logo.5d5d9eef.svg"
+  }
+]);
