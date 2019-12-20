3bf1ca1597d52f6c2404f32e8dbc0052f725bc9c
ehaffey <e-haffey@hotmail.com>
2019-12-20 16:43:02 -0500

    Login revamp
    --Completely changed layout and login flow/logic in src folder
    using files from auth0 docs

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
-import {
-  withRouter,
-  Route,
-  Switch
-  //Link
-} from 'react-router-dom';
-//import logo from './logo.svg';
-import './App.css';
-import NavBar from './NavBar/NavBar';
-import Callback from './Callback';
-import auth0Client from './Auth'
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import { Container } from "reactstrap";
 
-class App extends Component {
-  async componentDidMount() {
-    if (this.props.location.pathname === '/callback') return;
-    try {
-      await auth0Client.silentAuth();
-      this.forceUpdate();
-    } catch (err) {
-      if (err.error !== 'login_required') console.log(err.error);
-    }
+import PrivateRoute from "./components/PrivateRoute";
+import Loading from "./components/Loading";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./views/Home";
+import Profile from "./views/Profile";
+import { useAuth0 } from "./react-auth0-spa";
+import history from "./utils/history";
+
+// styles
+import "./App.css";
+
+// fontawesome
+import initFontAwesome from "./utils/initFontAwesome";
+initFontAwesome();
+
+const App = () => {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <Loading />;
   }
 
-  render() {
-    return (
-      <div>
-        <NavBar/>
-        <Switch>
-         <Route exact path='/callback'>
-           <Callback />
-         </Route>
-       </Switch>
+  return (
+    <Router history={history}>
+      <div id="app" className="d-flex flex-column h-100">
+        <NavBar />
+        <Container className="flex-grow-1 mt-5">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <PrivateRoute path="/profile" component={Profile} />
+          </Switch>
+        </Container>
+        <Footer />
       </div>
-    );
-  }
-}
+    </Router>
+  );
+};
 
-export default withRouter(App);
+export default App;
